6386c89ceab9d3d24b419876634ad28d58fe1b8a
SilverCock <lichikovwork@gmail.com>
2025-06-01 18:37:20 +0700

    Tracking 1
    Almost done tracking script, only preprocess and logging remained.

--- bubbles_tracking.py
+++ bubbles_tracking.py
@@ -2,38 +2,71 @@ import os
 import cv2
 import numpy as np
 import pandas as pd
+import torch
 from ultralytics import YOLO
 
 # Parameters
-MODEL_DIR = "\\path\\to\\seg\\model"
-VIDEO_DIR = "\\path\\to\\source\\video"
+MODEL_DIR = "\\path\\to\\seg\\model.yaml"
+VIDEO_DIR = "\\path\\to\\source\\video.avi" #must load 200x200 video of fiber, not raw 1024x200 video
+OUTPUT_DIR = "\\path\\to\\output\\file.csv"
 
 model = YOLO(MODEL_DIR)
 
-#create output csv file
+#create output file
+os.mkdir(OUTPUT_DIR)
 
-# Loading video
-def load_video(VIDEO_DIR):
-    print('Loading video')
-    # Only load video from path
+# load video
+cap = cv2.VideoCapture(VIDEO_DIR)
+frame_idx = 0
+records = []
+
+if not cap.isOpened():
+    print("Error opening video file")
 
 # Preprocess
-def preprocess():
-    print('delet me')
+def preprocess(frame):
+    #someone did not commit his preprocess function
+    return filtered_frame
     
-#while true read video frame by frame
+#main loop
+while cap.isOpened():
+    success, frame = cap.read()
+    if not success:
+        break
+    
+    filtered_frame = preprocess(frame)
 
-    #preprocess frame
-    #model.track
+    results = model.track(source=filtered_frame, persist=True, imgsz=200, device=0, stream=True, verbose=False)
+    
+    if results and results[0].boxes is not None:
+        r = results[0]
 
-    #working with mask
-        #obj_id
-        #center
-        #area
-        #eq_radius
-         
-    #record to csv file
+        for i in range(len(r.boxes)):
 
-    #frame_idx+=1
+            #working with bbox -- get ID, class, center coordinates
+            if r.boxes and r.boxes.data is not None:
+                obj_id = int(r.boxes.id[i])
+                cls = int(r.boxes.cls[i])
+                x1, y1, x2, y2 = r.boxes.xyxy[i].cpu().numpy()
+                cx = (x1 + x2) / 2
+                cy = (y1 + y2) / 2
+            else:
+                obj_id, cls, cx, cy = None, None, None, None
 
-    #maybe some logging
+            #working with mask -- get area an radius
+            if r.masks and r.masks.data is not None:
+                mask = r.masks.data[i].cpu().numpy()
+                area = np.sum(mask)
+                radius = np.sqrt(area / np.pi)
+            else:
+                area, radius = None, None
+
+            #udpating records array 
+            records.append({"id": obj_id, "frame": frame_idx, "cx": cx, "cy": cy, "area": area, "eq_radius": radius})
+
+    frame_idx += 1
+
+cap.release()
+
+df = pd.DataFrame(records)
+df.to_csv(OUTPUT_DIR)
